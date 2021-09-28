60c772484a9a073fda133b503e786331965995ef
tmavroeidakos <thodoris.mavroeidakos@avlos.ai>
2021-09-28 10:26:32 +0300

    fix: remove redundant import for Address.sol.

--- contracts/FarmSwap.sol
+++ contracts/FarmSwap.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 
 import "./FarmCoin.sol";
 import "./Staking.sol";
