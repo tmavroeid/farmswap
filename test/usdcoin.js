d64fcc67285d9f804911ee6109191f87829360d8
tmavroeidakos <thodoris.mavroeidakos@avlos.ai>
2021-09-28 09:54:55 +0300

    feat: add tests for USDCoin contract.

--- test/usdcoin.js
+++ test/usdcoin.js
@@ -12,38 +12,80 @@ contract("USDCoin", async accounts => {
     });
 
     it("can add USDC name and symbol properly", async() => {
-        
+        assert.equal("USD Coin", await instance.name.call());
+        assert.equal("USDC", await instance.symbol.call());
     });
 
     it("minting", async() => {
-        
+        // use account 1 since that account should have 0 
+        let intial_balance = await instance.balanceOf(accounts[3]);
+        // verify the balance is 0 USDC
+        assert.equal(intial_balance.toNumber(), 0, "intial balance for account 1 should be 0")
+
+        await instance.mint(accounts[3], 10);
+        let after_balance = await instance.balanceOf(accounts[3]);
+        assert.equal(after_balance.toNumber(), 10, "The balance after minting 100 should be 100")
     });
 
     it("minting to 0 address", async()=>{
-        
+        try {
+            // Mint with address 0
+            await instance.mint('0x0000000000000000000000000000000000000000', 100);
+        }catch(error){
+            assert.equal(error.reason, "USDC: mint to the zero address", "Failed minting on zero address")
+        }
     });
 
     it("minting 0 amount of USDC", async()=>{
-        
+        instance = await USDCoin.deployed();
+        try {
+            await instance.mint(accounts[2], 0);
+        }catch(error){
+            assert.equal(error.reason, "USDC: mint 0 amount")
+        }
     });
 
     it("burning", async() => {
-        
+        await instance.mint(accounts[1], 100);
+        try {
+            await instance.burn(accounts[1], 50);
+        }catch(error){
+            assert.fail(error);
+        }
+        let balance = await instance.balanceOf(accounts[1]);
+
+        assert.equal(balance.toNumber(), 50, "Burning 50 should reduce users balance to 750")
     });
 
     it("burning to 0 address", async()=>{
-        
+        try{
+            await instance.burn('0x0000000000000000000000000000000000000000', 100);
+        }catch(error){
+            assert.equal(error.reason, "USDC: burn from the zero address", "Failed to notice burning on 0 address")
+        }
     });
 
     it("burning more amount of USDC than the amount of balance", async()=>{
-        
+        try {
+            await instance.burn(accounts[1], 10000);
+        }catch(error){
+            assert.equal(error.reason, "USDC: burn amount exceeds balance", "Failed to capture too big burns on an account")
+        }
     });
 
     it("can transfer USDC from one account to another", async()=>{
-        
+        await instance.mint(accounts[5], 200);
+        await instance.transfer(accounts[4],100, {from:accounts[5]});
+        let balance = await instance.balanceOf(accounts[4]);
+        assert.equal(balance.toNumber(), 100, "The amount of USDC should be 100")
     });
 
     it("cannot transfer more USDC from one account than existing", async()=>{
-        
+        await instance.mint(accounts[5], 200);
+        try {
+            await instance.transfer(accounts[4],300, {from:accounts[5]});
+        }catch(error){
+            assert.equal(error.reason, "USDC: tokens should be less/equal to the balance of the sender")
+        }
     });
 });
